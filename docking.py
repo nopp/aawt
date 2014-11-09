9b2fc52ff68cd9a854c50821b62dc14289ee1c70
root <root@carlosmalucelli.com>
2014-11-08 19:54:58 -0500

    new methods

--- docking.py
+++ docking.py
@@ -17,23 +17,42 @@ def index():
 # Version
 @app.route("/version/<dockerHost>", methods=['GET'])
 def version(dockerHost):
-	dApi = dockerApi()
-	rtn = dApi.docker_version(dockerHost)
-	return "Docker Version: "+json.loads(rtn)['Version']
+	if request.method == 'GET':
+		dApi = dockerApi()
+		rtn = dApi.docker_version(dockerHost)
+		return rtn
+
+# Info
+@app.route("/info/<dockerHost>", methods=['GET'])
+def info(dockerHost):
+	if request.method == 'GET':
+		dApi = dockerApi()
+		rtn = dApi.docker_info(dockerHost)
+		return rtn
 
 # Containers
 @app.route("/containers/<dockerHost>", methods=['GET'])
 def containers(dockerHost):
-	dApi = dockerApi()
-	rtn = dApi.docker_containers(dockerHost)
-	return rtn
+	if request.method == 'GET':
+		dApi = dockerApi()
+		rtn = dApi.docker_containers(dockerHost)
+		return rtn
 
 # Container Info
 @app.route("/container/<dockerHost>/<idContainer>", methods=['GET'])
 def containerInfo(dockerHost,idContainer):
-	dApi = dockerApi()
-	rtn = dApi.docker_container_info(dockerHost,idContainer)
-	return rtn
+	if request.method == 'GET':
+		dApi = dockerApi()
+		rtn = dApi.docker_container_info(dockerHost,idContainer)
+		return rtn
+
+# Container start test
+@app.route("/start", methods=['POST'])
+def containerStart():
+	if request.method == 'POST':
+		dApi = dockerApi()
+		rtn = dApi.docker_container_start(request.form['dockerHost'],request.form['idContainer'])
+		return rtn
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=8282,debug=True)
--- lib/docker_api.py
+++ lib/docker_api.py
@@ -7,16 +7,24 @@ from urllib2 import Request, urlopen, URLError
 
 class dockerApi():
 
-	def urlRequest(self,dockerAgent,action):
+	def urlRequest(self,dockerAgent,action,mtd=None):
 		uName = "carlos"
 		pWord = "loke99"
 		userData = "Basic " + (uName + ":" + pWord).encode("base64").rstrip()
 		try:
-			req = urllib2.Request('http://'+str(dockerAgent)+'/'+str(action))
-			req.add_header('Accept', 'application/json')
-			req.add_header("Content-type", "application/x-www-form-urlencoded")
-			req.add_header('Authorization', userData)
-			return req
+			if mtd == "POST":
+				req = urllib2.Request('http://'+dockerAgent+'/'+action)
+				req.get_method = lambda: 'POST'
+				req.add_header('Accept', 'application/json')
+				req.add_header("Content-type", "application/x-www-form-urlencoded")
+				req.add_header('Authorization', userData)
+				return req
+			else:
+				req = urllib2.Request('http://'+dockerAgent+'/'+action)
+				req.add_header('Accept', 'application/json')
+				req.add_header("Content-type", "application/x-www-form-urlencoded")
+				req.add_header('Authorization', userData)
+				return req
 		except URLError, e:
 			return e.reason
 
@@ -27,6 +35,16 @@ class dockerApi():
 			return rtn.read()
 		except URLError, e:
 			return e.reason
+
+	def docker_info(self,dockerHost):
+		try:
+			req = self.urlRequest(dockerHost,"info")
+			rtn = urllib2.urlopen(req,timeout = 2)
+			return rtn.read()
+		except URLError, e:
+			return e.reason
+
+	# Container methods
 
 	def docker_containers(self,dockerHost):
 		try:
@@ -39,6 +57,30 @@ class dockerApi():
 	def docker_container_info(self,dockerHost,idContainer):
 		try:
 			req = self.urlRequest(dockerHost,"containers/"+idContainer+"/json")
+			rtn = urllib2.urlopen(req,timeout = 2)
+			return rtn.read()
+		except URLError, e:
+			return e.reason
+
+	def docker_container_start(self,dockerHost,idContainer):
+		try:
+			req = self.urlRequest(dockerHost,"containers/"+idContainer+"/start","POST")
+			rtn = urllib2.urlopen(req,timeout = 2)
+			return rtn.read()
+		except URLError, e:
+			return e.reason
+
+	def docker_container_stop(self,dockerHost,idContainer):
+		try:
+			req = self.urlRequest(dockerHost,"containers/"+idContainer+"/stop","POST")
+			rtn = urllib2.urlopen(req,timeout = 2)
+			return rtn.read()
+		except URLError, e:
+			return e.reason
+
+	def docker_container_restart(self,dockerHost,idContainer):
+		try:
+			req = self.urlRequest(dockerHost,"containers/"+idContainer+"/restart","POST")
 			rtn = urllib2.urlopen(req,timeout = 2)
 			return rtn.read()
 		except URLError, e:
