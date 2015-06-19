ae543de21ce0d9509c143a0618a2e9f794566e0f
Carlos Augusto Malucelli <carlos.malucelli@preventsenior.com.br>
2015-06-19 14:02:07 -0300

    fix

--- lib/atw.py
+++ lib/atw.py
@@ -14,41 +14,49 @@ class Atw:
 	region = "sa-east-1"
 
 	def ec2_connect(self):
-		access_key = config.get('conf','accessKey')
-		secret_key = config.get('conf','secretKey')
-		return boto.ec2.connect_to_region(self.region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
+		try:
+			access_key = config.get('conf','accessKey')
+			secret_key = config.get('conf','secretKey')
+			conn = boto.ec2.connect_to_region(self.region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
+			return conn
+		except:
+			print "Error - can't connect to EC2"
 
 	# EC2 Search by IP (Private) 
 	def searchByPrIP(self,ip):
 		ec2_conn = self.ec2_connect()
-		filters = {"private-ip-address": ip}
-		ec2List = []
-		for ec2 in ec2_conn.get_only_instances(filters=filters):
-			ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
-			ec2List.append(ec2Vm)
-		return ec2List
+		try:
+			filters = {"private-ip-address": ip}
+			ec2List = []
+			for ec2 in ec2_conn.get_only_instances(filters=filters):
+				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
+				ec2List.append(ec2Vm)
+			return ec2List
+		except:
+			print "Error - Can't search EC2 by Private IP"
 
 	# EC2 Search by IP (Public)
 	def searchByPIP(self,ip):
 		ec2_conn = self.ec2_connect()
-		filters = {"ip-address": ip}
-		ec2List = []
-		for ec2 in ec2_conn.get_only_instances(filters=filters):
-			ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
-			ec2List.append(ec2Vm)
-		return ec2List
+		try:
+			filters = {"ip-address": ip}
+			ec2List = []
+			for ec2 in ec2_conn.get_only_instances(filters=filters):
+				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
+				ec2List.append(ec2Vm)
+			return ec2List
+		except:
+			print "Error - Can't search EC2 by Public IP"
 
 	# EC2 Search by TAG
 	def searchByTAG(self,tagKey,tagValue):
 		ec2_conn = self.ec2_connect()
-		filters = {"tag-key":tagKey,"tag-value":"*"+tagValue+"*"}
-		ec2List = []
-		for ec2 in ec2_conn.get_only_instances(filters=filters):
-			ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
-			ec2List.append(ec2Vm)
-		return ec2List
-
-	def ec2_list(self):
-		ec2_conn = ec2_connect()
-		for ec2 in ec2_conn.get_only_instances():
-			return ec2.tags['Name']+" "+ec2.id+" "+ec2.private_ip_address+" "+ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType']+" ("+ec2.state+")"
+		try:
+			filters = {"tag-key":tagKey,"tag-value":"*"+tagValue+"*"}
+			ec2List = []
+			for ec2 in ec2_conn.get_only_instances(filters=filters):
+				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
+				ec2List.append(ec2Vm)
+			return ec2List
+		except:
+			print "Error - Can't search EC2 by TAG"
