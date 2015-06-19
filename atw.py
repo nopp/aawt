22073c7f56044a877ba1a69052a3d6d004d9c4a8
Carlos Augusto Malucelli <carlos.malucelli@preventsenior.com.br>
2015-06-19 14:15:53 -0300

    fix

--- atw.py
+++ atw.py
@@ -15,24 +15,30 @@ def index():
 # EC2 SearchByIP
 @app.route("/ec2_searchByIP", methods=['POST'])
 def ec2_searchByIP():
-	if request.method == 'POST':	
-		atw = Atw()
-		if request.form['iptype'] == "public":
-			rtn = atw.searchByPIP(request.form['ip'])
-			total = len(rtn)
-		else:
-			rtn = atw.searchByPrIP(request.form['ip'])
-			total = len(rtn)
-	return render_template('result.html',results=rtn,total=total)
+	try:
+		if request.method == 'POST':	
+			atw = Atw()
+			if request.form['iptype'] == "public":
+				rtn = atw.searchByPIP(request.form['ip'])
+				total = len(rtn)
+			else:
+				rtn = atw.searchByPrIP(request.form['ip'])
+				total = len(rtn)
+		return render_template('result.html',results=rtn,total=total)
+	except:
+		print "Error - Can't search EC2 by IP"
 
 # EC2 SearchByTAG
 @app.route("/ec2_searchByTAG", methods=['POST'])
 def ec2_searchByTAG():
-	if request.method == 'POST':	
-		atw = Atw()
-		rtn = atw.searchByTAG(request.form['tagKey'],request.form['tagValue'])
-		total = len(rtn)
-	return render_template('result.html',results=rtn,total=total)
+	try:
+		if request.method == 'POST':	
+			atw = Atw()
+			rtn = atw.searchByTAG(request.form['tagKey'],request.form['tagValue'])
+			total = len(rtn)
+		return render_template('result.html',results=rtn,total=total)
+	except:
+		print "Error - Can'tsearch EC2 by TAG"
 
 if __name__ == '__main__':
 	app.run(host='172.173.4.74',port=8082,debug=True)
