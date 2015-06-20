f2c383c22346da358cb47dd443526bca31e7444f
root <root@nopp.lab.br>
2015-06-19 22:29:56 -0300

    Fix

--- lib/atw.py
+++ lib/atw.py
@@ -11,42 +11,38 @@ config.read('/etc/atw/config.cfg')
 
 class Atw:
 
-	region = "sa-east-1"
+	# This is a little hammer, I'll fix this soon :D
+	def error(self,message):
+		error = []
+		eMsg = [message]
+		error.append(eMsg)
+		return error
 
 	def ec2_connect(self):
 		try:
 			access_key = config.get('conf','accessKey')
 			secret_key = config.get('conf','secretKey')
-			conn = boto.ec2.connect_to_region(self.region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
+			region = config.get('conf','region')
+			conn = boto.ec2.connect_to_region(region,aws_access_key_id=access_key,aws_secret_access_key=secret_key)
 			return conn
-		except:
-			print "Error - can't connect to EC2 (LIB)"
+		except:			
+			return self.error("Error - can't connect to EC2 (LIB)")
 
-	# EC2 Search by IP (Private) 
-	def searchByPrIP(self,ip):
+	# EC2 Search by IP (Private/Public) 
+	def searchByIP(self,ip,iptype):
 		ec2_conn = self.ec2_connect()
 		try:
-			filters = {"private-ip-address": ip}
+			if iptype == "private":
+				filters = {"private-ip-address": ip}
+			else:
+				filters = {"ip-address": ip}
 			ec2List = []
 			for ec2 in ec2_conn.get_only_instances(filters=filters):
 				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
 				ec2List.append(ec2Vm)
 			return ec2List
 		except:
-			print "Error - Can't search EC2 by Private IP (LIB)"
-
-	# EC2 Search by IP (Public)
-	def searchByPIP(self,ip):
-		ec2_conn = self.ec2_connect()
-		try:
-			filters = {"ip-address": ip}
-			ec2List = []
-			for ec2 in ec2_conn.get_only_instances(filters=filters):
-				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state]
-				ec2List.append(ec2Vm)
-			return ec2List
-		except:
-			print "Error - Can't search EC2 by Public IP (LIB)"
+			return self.error("Error - Can't search EC2 by IP (LIB)")
 
 	# EC2 Search by TAG
 	def searchByTAG(self,tagKey,tagValue):
@@ -59,4 +55,4 @@ class Atw:
 				ec2List.append(ec2Vm)
 			return ec2List
 		except:
-			print "Error - Can't search EC2 by TAG (LIB)"
+			return self.error("Error - Can't search EC2 by TAG (LIB)")
