c54745f0de0d2b1b681dab4acb5fed18720a1a18
root <root@server02.lab.br>
2015-06-22 18:40:20 -0300

    tag hidden :)

--- lib/atw.py
+++ lib/atw.py
@@ -56,13 +56,20 @@ class Atw:
 		except:
 			return self.error("Error - Can't search EC2 by TAG (LIB)")
 
+	def returnTags(self,tags):
+		ec2Tags = []
+		for tagKey,tagValue in tags.iteritems():
+			ec2Tags.append(tagKey+":"+tagValue)
+		return ec2Tags
+
 	# EC2 List all
 	def ec2_listAll(self):
 		ec2_conn = self.ec2_connect()
 		try:
 			ec2List = []
 			for ec2 in ec2_conn.get_only_instances():
-				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state,ec2.placement]
+				tagLists = self.returnTags(ec2.tags)
+				ec2Vm = [ec2.tags['Name'],ec2.id,ec2.private_ip_address,ec2_conn.get_instance_attribute(ec2.id,"instanceType")['instanceType'],ec2.state,ec2.placement,tagLists]
 				ec2List.append(ec2Vm)
 			return ec2List
 		except:
