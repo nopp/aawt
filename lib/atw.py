d09035642df3773980ce777419b263a3972bc94a
Carlos Augusto Malucelli <nopp@pia-mini.local>
2016-03-19 22:15:35 -0300

    New function ebs(size and total)

--- lib/atw.py
+++ lib/atw.py
@@ -13,7 +13,7 @@ class Atw:
     access_key = config.get('conf','accessKey')
     secret_key = config.get('conf','secretKey')    
     
-    # This is a little hammer, I'll fix this soon :D
+    # This is a little hammer, I'll fix this soon :D    
     def menu(self):
         menu = config.get('conf','regions')
         return menu.split(",")    
@@ -25,22 +25,22 @@ class Atw:
         error.append(eMsg)
         return error
 
-    # Connect to EC2
-    def ec2_connect(self,region):
+    # Connect to resource
+    def connect_resource(self,region,resource):
         try:
             session = boto3.session.Session(aws_access_key_id=self.access_key,aws_secret_access_key=self.secret_key,region_name=region)
-            conn = session.resource('ec2')
+            conn = session.resource(resource)
             return conn
         except:			
-            return self.error("Error - can't connect to EC2 (LIB)")
+            return self.error("Error - can't connect to "+resource+" (LIB)")
 
-    # Connect to RDS
-    def rds_connect(self,region):
+    # Connect to client
+    def connect_client(self,region,client):
         try:
-            conn = boto3.client('rds',aws_access_key_id=self.access_key,aws_secret_access_key=self.secret_key,region_name=region)
+            conn = boto3.client(client,aws_access_key_id=self.access_key,aws_secret_access_key=self.secret_key,region_name=region)
             return conn
         except:
-            return self.error("Error - can't connect to RDS (LIB)")
+            return self.error("Error - can't connect to "+client+" (LIB)")
 
     # Return EC2 Tags
     def returnTags(self,tags):
@@ -59,9 +59,9 @@ class Atw:
 
     # EC2 List all
     def ec2_listAll(self,region):
-        ec2_conn = self.ec2_connect(region)
+        ec2_res = self.connect_resource(region,"ec2")
         ec2List = []
-        for ec2 in ec2_conn.instances.all():
+        for ec2 in ec2_res.instances.all():
             tagList = self.returnTags(ec2.tags)
             ec2Vm = [self.returnTagEC2(tagList,"Name"),ec2.instance_id,ec2.private_ip_address,ec2.instance_type,ec2.state['Name'],ec2.placement['AvailabilityZone'],tagList,ec2.public_ip_address,ec2.platform]
             ec2List.append(ec2Vm)
@@ -69,15 +69,31 @@ class Atw:
 
     # EC2 total
     def ec2_total(self,region):
-        ec2_conn = self.ec2_connect(region)
+        ec2_res = self.connect_resource(region,"ec2")
         total = 0
-        for ec2 in ec2_conn.instances.all():
+        for ec2 in ec2_res.instances.all():
             total=total+1
         return total
+    
+    # EC2 total EBS
+    def ec2_totalEbs(self,region):
+        ec2 = self.connect_client(region,"ec2")
+        total = 0
+        for volume in ec2.describe_volumes()['Volumes']:
+            total = total+1
+        return total
+    
+    # EC2 total size EBS
+    def ec2_totalSizeEbs(self,region):
+        ec2 = self.connect_client(region,"ec2")
+        size = 0
+        for volume in ec2.describe_volumes()['Volumes']:
+            size = size+volume['Size']
+        return size
 
     # RDS List all
     def rds_listAll(self,region):
-        rdsClient = self.rds_connect(region)
+        rdsClient = self.connect_client(region,"rds")
         try:
             rdsList = []
             for rds in rdsClient.describe_db_instances()['DBInstances']:
