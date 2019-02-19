2434e3389984639781b63fe562fe4da5f662edf0
Carlos Augusto Malucelli <malucellicarlos@gmail.com>
2019-02-19 13:25:55 -0300

    Update run.sh
    Python3 support

--- docker/run.sh
+++ docker/run.sh
@@ -3,4 +3,4 @@ sed -i s/ipserver/0.0.0.0/ /etc/aawt/config.cfg
 sed -i s/akey/"$akey"/ /etc/aawt/config.cfg
 sed -i s/skey/"$skey"/ /etc/aawt/config.cfg
 
-/usr/bin/python aawt.py
+/usr/bin/python3 aawt.py
